d7a89bf0d005a3816cf6d23c985d9217ea21096d
Olagboye Seyi <akingbolaola@gmail.com>
2024-10-25 18:24:06 +0100

    Setting Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import SignIn from "./Components/SignIn/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <SignIn/>
+    </>
   );
 }
 
---
+++ src/Components/SignIn/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import mainSoldier from "../../assets/images/soldier-asset-1.svg";
+const SignIn = () => {
+  return (
+    <div>
+      <img src={mainSoldier} alt="" />
+    </div>
+  );
+};
+
+export default SignIn;
