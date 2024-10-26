a6019ebc681a79953417123a27333cdfddfd9fe8
Olagboye Seyi <akingbolaola@gmail.com>
2024-10-26 20:30:06 +0100

    Setting Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+// @ts-nocheck
 import "./App.css";
-import { PrivyProvider } from "@privy-io/react-auth";
+import { PrivyProvider, UseSolanaWalletsInterface } from "@privy-io/react-auth";
 import SignIn from "./Components/SignIn/SignIn";
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+//Solflare
+
+const solanaConnectors = toSolanaWalletConnectors({
+  // By default, shouldAutoConnect is enabled
+  shouldAutoConnect: true,
+});
 
 function App() {
+  // const solanaConnectors = UseSolanaWalletsInterface();
   return (
     <>
       <PrivyProvider
@@ -10,10 +19,16 @@ function App() {
         config={{
           loginMethods: ["wallet"],
           appearance: {
+            walletChainType: "solana-only",
             theme: "dark",
             accentColor: "#1E2211",
             logo: "https://www.onchainarmy.xyz/logo192.png",
-            landingHeader: 'Connect Wallet', 
+            landingHeader: "Connect Wallet",
+          },
+          externalWallets: {
+            solana: {
+              connectors: solanaConnectors,
+            },
           },
           // Create embedded wallets for users who don't have a wallet
           embeddedWallets: {
--- src/Components/SignIn/SignIn.tsx
+++ src/Components/SignIn/SignIn.tsx
@@ -1,12 +1,61 @@
-import React from "react";
+// @ts-nocheck
+import React, { useEffect, useState } from "react";
 import mainSoldier from "../../assets/images/soldier-asset-1.svg";
-import { usePrivy } from "@privy-io/react-auth";
+import { usePrivy, useLogin } from "@privy-io/react-auth";
+import { useSolanaWallets } from "@privy-io/react-auth/solana";
 import soldierVector from "../../assets/images/camo-background.svg";
 const SignIn = () => {
-  const { ready, authenticated, login, connectWallet } = usePrivy();
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const { ready, authenticated, connectWallet, user } = usePrivy();
+
+  const { wallets } = useSolanaWallets();
+  const wallet = wallets[0];
+  const address = wallet?.address;
+
+  const message = "This is the message I am signing";
+  const handleSignIn = async () => {
+    try {
+      // Ensure the wallet is connected
+      await connectWallet();
+      setIsWalletConnected(true);
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  };
+
+  const handleMessage = async () => {
+    try {
+      if (!wallet) {
+        throw new Error("Wallet not found.");
+      }
+      const signature = await wallet.signMessage(
+        new TextEncoder().encode(message)
+      );
+      console.log("Message signed with signature:", signature);
+    } catch (error) {
+      console.error("Error signing message:", error);
+    }
+  };
+
+  useEffect(() => {
+    // Only call handleMessage if the wallet is connected and the address is available
+    if (isWalletConnected && address) {
+      handleMessage();
+    }
+  }, [isWalletConnected, address]);
+
+  //   if (wallet) {
+  //     const { signature } = await wallet?.address.signMessage(
+  //       new TextEncoder().encode(message)
+  //     );
+  //     console.log(signature);
+  //   } else {
+  //     console.log("No wallet connected");
+  //   }
+  //console.log(signature);
   return (
     <div className="flex justify-center items-center h-screen">
-      <img src={soldierVector} alt="" className="absolute top-0" />
+      <img src={soldierVector} alt="" className="absolute top-0 w-[80%]" />
       <div className="flex justify-center items-center flex-col">
         <img
           src={mainSoldier}
@@ -15,9 +64,11 @@ const SignIn = () => {
         />
         <button
           className="bg-red-600 text-white py-4 px-8 text-[25px] font-soli hover:bg-red-700 transition mt-10 clip-button"
-          onClick={() => connectWallet({ walletList: ["detected_wallets"] })}
+          onClick={handleSignIn}
         >
-          Sign in Soldier!
+          {wallets && wallets[0]?.address
+            ? wallets[0]?.address
+            : " Sign in Soldier!"}
         </button>
       </div>
     </div>
