390d4ac3f689c994a2fb6e7c8b0d171c1ec0132c
Olagboye Seyi <akingbolaola@gmail.com>
2024-11-11 13:05:40 +0100

    Started implementing Dashboard design

--- src/Components/LeaderboardTable/LeaderboardTable.tsx
+++ src/Components/LeaderboardTable/LeaderboardTable.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import OverviewContent from "../OverviewContent/OverviewContent";
+import WalletBallance from "../WalletBallance/WalletBallance";
 
 const LeaderboardTable = () => {
-  return <div>LeaderboardTable</div>;
+  return (
+    <div>
+      LeaderboardTable
+    </div>
+  );
 };
 
 export default LeaderboardTable;
--- src/pages/Dashboard/Leaderboard.tsx
+++ src/pages/Dashboard/Leaderboard.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import LeaderboardTable from "../../Components/LeaderboardTable/LeaderboardTable";
+import WalletBallance from "../../Components/WalletBallance/WalletBallance";
+import OverviewContent from "../../Components/OverviewContent/OverviewContent";
 
 const Leaderboard = () => {
   return (
     <div>
+      <OverviewContent />
       <LeaderboardTable />
     </div>
   );
