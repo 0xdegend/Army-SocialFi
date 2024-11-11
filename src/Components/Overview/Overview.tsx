95754f78732d5076fdb387c5fd63956ac3cd8e88
Olagboye Seyi <akingbolaola@gmail.com>
2024-11-11 18:50:09 +0100

    Implementing Profile Stats

--- src/Components/Overview/Overview.tsx
+++ src/Components/Overview/Overview.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import WalletBallance from "../WalletBallance/WalletBallance";
 import { usePrivy } from "@privy-io/react-auth";
 import OverviewContent from "../OverviewContent/OverviewContent";
+import PointsOverview from "../PointsOverview/PointsOverview";
 const Overview = () => {
   const { user } = usePrivy();
   console.log(user);
@@ -9,6 +10,7 @@ const Overview = () => {
     <div className="w-full">
       <WalletBallance />
       <OverviewContent />
+      <PointsOverview />
     </div>
   );
 };
--- src/Components/OverviewContent/OverviewContent.tsx
+++ src/Components/OverviewContent/OverviewContent.tsx
@@ -4,14 +4,14 @@ import { usePrivy } from "@privy-io/react-auth";
 const OverviewContent = () => {
   const { user } = usePrivy();
   return (
-    <div className="bg-[#1D2211] p-3 relative clip-top-left-bottom-right">
+    <div className="bg-[#1D2211] p-5 relative clip-top-left-bottom-right">
       <img
         src={camoImage}
         alt="Army Camo"
-        className="absolute top-2 w-[40%] right-0"
+        className="absolute top-2.5 w-[50%] right-0"
       />
-      <div className="mx-auto flex flex-col justify-center items-center mt-10">
-        <div className="flex flex-col justify-center items-center">
+      <div className="mx-auto flex mt-5">
+        <div className="flex gap-5">
           <div className="h-[100px] w-[100px] rounded-[50%] bg-[#68604B] flex justify-center items-center">
             <div className="h-[90px] w-[90px] rounded-[50%] bg-[#E1DDCC] flex justify-center items-center">
               <img
@@ -21,9 +21,15 @@ const OverviewContent = () => {
               />
             </div>
           </div>
-          <h5 className="font-soli text-[24px] text-white mt-5">
-            Gm, General {`${user?.twitter?.name}`}
-          </h5>
+          <div>
+            <h5 className="font-soli text-[24px] text-white mt-5 capitalize">
+              Gm,{" "}
+              <span className="text-[#FFED41] capitalize">{`${user?.twitter?.name}`}</span>
+            </h5>
+            <h5 className="mt-3 cursor-pointer font-soli text-[#F83726]">
+              {user?.wallet?.address}
+            </h5>
+          </div>
         </div>
       </div>
     </div>
