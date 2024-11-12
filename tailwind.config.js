beef201c40b83b1daf96d78b02c61cc7fd07269b
daniekeys <starlordflash@gmail.com>
2024-11-12 04:47:43 +0100

    pushed the leaderboard tables

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         soli: ["Soli Px W01 Regular", "regular"], // Add your custom handwriting font
       },
+      colors: {
+        ash: "rgba(63, 63, 70, 0.6)",
+        grey2: "#3f3f4699",
+        dark: "#1f1f1f",
+        primary: "#1D2211",
+        secondary: "#4B5320",
+      },
     },
   },
   plugins: [],
