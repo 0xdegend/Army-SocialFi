f01bb4491c5da5e8bb71f474d5be786a3f5427a9
daniekeys <starlordflash@gmail.com>
2024-11-13 11:53:09 +0100

    updated the mobile resp

--- src/Components/utils/buttons/SideBarButton.tsx
+++ src/Components/utils/buttons/SideBarButton.tsx
@@ -2,36 +2,31 @@ import React, { FC } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { ISidebarButtonName } from "../../../types";
 
-const SideBarButton: FC<{ name: ISidebarButtonName }> = ({ name }) => {
+const SideBarButton: FC<{ name: ISidebarButtonName; url: string }> = ({
+  name,
+  url,
+}) => {
   const location = useLocation();
 
   return (
     <>
       {name ? (
-        <>
-          <Link
-            to={`/dashboard${name !== "dashboard" ? `/${name}` : ""}`}
-            className={`text-white font-soli text-[14px] capitalize transition-all duration-75  ${
-              location.pathname ===
-              `/dashboard${name !== "dashboard" ? `/${name}` : ""}`
-                ? "bg-[#4B5320] text-opacity-100"
-                : "text-opacity-50"
-            }  flex items-center gap-2 px-2 rounded-[8px] h-[50px]`}
-          >
-            <span
-              className={`${
-                location.pathname ===
-                `/dashboard${name !== "dashboard" ? `/${name}` : ""}`
-                  ? "opacity-100"
-                  : "opacity-50"
-              }`}
-            ></span>
-            <span>{name}</span>
-          </Link>
-        </>
-      ) : (
-        <></>
-      )}
+        <Link
+          to={`/${url}`}
+          className={`text-white font-soli text-[14px] capitalize transition-all duration-75 ${
+            location.pathname === `/${url}`
+              ? "bg-[#4B5320] text-opacity-100"
+              : "text-opacity-50"
+          } flex items-center gap-2 px-2 rounded-[8px] h-[50px]`}
+        >
+          <span
+            className={`${
+              location.pathname === `/${url}` ? "opacity-100" : "opacity-50"
+            }`}
+          ></span>
+          <span>{name}</span>
+        </Link>
+      ) : null}
     </>
   );
 };
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,25 +3,21 @@ import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import armyBackground from "../../assets/images/army-background.webp";
 import SideBar from "../../Components/SideBar/SideBar";
+import DashboardLayout from "../../layout/DashboardLayout";
+import WalletBallance from "../../Components/WalletBallance/WalletBallance";
+import { usePrivy } from "@privy-io/react-auth";
+import OverviewContent from "../../Components/OverviewContent/OverviewContent";
+import PointsOverview from "../../Components/PointsOverview/PointsOverview";
 const Dashboard = () => {
   return (
-    <>
-      <div className="min-h-[100vh]  overflow-x-hidden flex relative ">
-        <div className="flex-1 bg-[#1E2211FF] lg:fixed lg:max-w-[250px]">
-          <SideBar />
-        </div>
-        <div
-          className={`bg-cover bg-center h-screen flex-[6]  pt-10 pb-5 ml-[250px]`}
-          style={{
-            backgroundImage: `url(${armyBackground})`,
-          }}
-        >
-          <div className="w-[97%] mx-auto lg:h-screen flow-hide">
-            <Outlet />
-          </div>
-        </div>
+    <DashboardLayout>
+      <div className="w-full h-full flex flex-col ">
+        <WalletBallance />
+        <OverviewContent />
+        <PointsOverview />
+       
       </div>
-    </>
+    </DashboardLayout>
   );
 };
 
