8bab79e659f0dfafdd99a12a66eac3b71445b97a
Olagboye Seyi <akingbolaola@gmail.com>
2024-11-14 15:39:10 +0100

    Updating UI

--- src/pages/Dashboard/Leaderboard.tsx
+++ src/pages/Dashboard/Leaderboard.tsx
@@ -11,40 +11,36 @@ const Leaderboard = () => {
   const [isGeneral, setIsGeneral] = useState(true);
   return (
     <DashboardLayout current={2}>
-      <div
-        className="w-full h-full"
-      
-      >
-
-      <div className="w-full h-full pt-20 xl:px-6 ">
-        <OverviewContent />
-        <div className="mt-8 pb-12">
-          <div className="flex items-center justify-start font-soli mb-8 gap-5">
-            <button
-              onClick={() => setIsGeneral(true)}
-              className={`px-6 h-10 flex items-center justify-center w-fit cursor-pointer font-medium ${
-                isGeneral
-                ? "text-white  rounded-md bg-primary "
-                : "border-[2px] border-primary rounded-md "
-              }`}
+      <div className="w-full h-full">
+        <div className="w-full h-[100vh] pt-20 xl:px-6 ">
+          <OverviewContent />
+          <div className="mt-8 pb-12">
+            <div className="flex items-center justify-start font-soli mb-8 gap-5">
+              <button
+                onClick={() => setIsGeneral(true)}
+                className={`px-6 h-10 flex items-center justify-center w-fit cursor-pointer font-medium ${
+                  isGeneral
+                    ? "text-white  rounded-md bg-primary "
+                    : "border-[2px] border-primary rounded-md "
+                }`}
               >
-              General LeaderBoard
-            </button>
-            <button
-              onClick={() => setIsGeneral(false)}
-              className={`px-6 h-10 flex items-center justify-center w-fit cursor-pointer  ${
-                !isGeneral
-                ? "text-white font-medium  rounded-md bg-primary "
-                : "border-[2px] border-primary rounded-md font-semibold "
-              }`}
+                General LeaderBoard
+              </button>
+              <button
+                onClick={() => setIsGeneral(false)}
+                className={`px-6 h-10 flex items-center justify-center w-fit cursor-pointer  ${
+                  !isGeneral
+                    ? "text-white font-medium  rounded-md bg-primary "
+                    : "border-[2px] border-primary rounded-md font-semibold "
+                }`}
               >
-              Campaign LeaderBoard
-            </button>
+                Campaign LeaderBoard
+              </button>
+            </div>
+            {isGeneral && <LeaderboardTable data={generalLeaderBoardData} />}
+            {!isGeneral && <LeaderboardTable data={campaignLeaderBoardData} />}
           </div>
-          {isGeneral && <LeaderboardTable data={generalLeaderBoardData} />}
-          {!isGeneral && <LeaderboardTable data={campaignLeaderBoardData} />}
         </div>
-              </div>
       </div>
     </DashboardLayout>
   );
