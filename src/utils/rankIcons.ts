72844cfd56ec267f3a078afedb9856a36b5ac0be
Olagboye Seyi <akingbolaola@gmail.com>
2024-11-25 18:25:50 +0100

    Updated Auth flow

--- src/utils/rankIcons.ts
+++ src/utils/rankIcons.ts
@@ -2,7 +2,9 @@
 
 import generalIcon from "../assets/images/general-rank.PNG";
 import captainIcon from "../assets/images/captain-rank.PNG";
-import sergeantIcon from "../assets/images/master-sergeant-rank.PNG";
+import masterChiefIcon from "../assets/images/master-chief-rank.PNG";
+import sergeantIcon from "../assets/images/sergeant-rank.PNG";
+import majorIcon from "../assets/images/major-rank.PNG";
 import corporalIcon from "../assets/images/corporal-rank.PNG";
 import privateIcon from "../assets/images/private-rank.PNG";
 
@@ -11,6 +13,8 @@ const rankIcons = {
   Captain: captainIcon,
   Sergeant: sergeantIcon,
   Corporal: corporalIcon,
+  Major: majorIcon,
+  Master: masterChiefIcon,
   Private: privateIcon,
 };
 
