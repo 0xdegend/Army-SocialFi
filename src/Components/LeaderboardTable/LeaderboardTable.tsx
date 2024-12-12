1ca10e958b0cc5d05c9e73c4eb34a86d9dd339a8
daniekeys <starlordflash@gmail.com>
2024-12-12 19:48:18 +0100

    pushed pagination fixes

--- src/Components/LeaderboardTable/LeaderboardTable.tsx
+++ src/Components/LeaderboardTable/LeaderboardTable.tsx
@@ -31,15 +31,15 @@ const LeaderboardTable = ({
     const filtered = !query
       ? data
       : data.filter((item: any) =>
-          isGeneral
-            ? item?.twitterUsername
-                ?.toLowerCase()
-                ?.includes(query.toLowerCase())
-            : item?.userId?.twitterUsername
-                ?.toLowerCase()
-                ?.includes(query.toLowerCase())
-        );
-    setCurrentData(filtered);
+        isGeneral
+          ? item?.twitterUsername
+            ?.toLowerCase()
+            ?.includes(query.toLowerCase())
+          : item?.userId?.twitterUsername
+            ?.toLowerCase()
+            ?.includes(query.toLowerCase())
+      );
+    setFilteredData(filtered);
    
 
     if (
@@ -66,6 +66,7 @@ const LeaderboardTable = ({
      
     }
   }, [data]);
+ 
 
   return (
     <div className="w-full font-inconsolata text-[20px] font-[600] clip-top-left-bottom-right flex flex-col bg-primary rounded-md p-3 lg:p-4  flow-hide ">
