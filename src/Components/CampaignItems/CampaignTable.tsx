48bbf20ee9ade9cc1e240480676ea5a98cf54e5f
Olagboye Seyi <akingbolaola@gmail.com>
2024-12-16 12:24:05 +0100

    Implementing Export Winners

--- src/Components/CampaignItems/CampaignTable.tsx
+++ src/Components/CampaignItems/CampaignTable.tsx
@@ -231,7 +231,7 @@ const SingleRow = ({
    
     // Generate and download CSV
     const csvData = convertToCSV(result);
-    downloadCSV(csvData, `${item?.name}.csv`);
+    downloadCSV(csvData, `${item?.nam}.csv`);
   };
   return (
     <tr
